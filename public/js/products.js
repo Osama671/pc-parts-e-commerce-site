25bdc7d8e2afbf8f1c0c1d96c0c8aad8258d5def
Osama <osamaazar48@hotmail.com>
2023-10-17 09:41:56 -0400

    All container items wraped; no more rows created

--- public/js/products.js
+++ public/js/products.js
@@ -4,27 +4,11 @@ let rowNum = 1
 function getProductResults() {
   $.getJSON('../api/products.json', function (element) {
     element.forEach((arr) => {
-      if (counter === 4) {
-        // Create a new bootstrap row after every 4th result and in the very beginning.
-        // Row IDs created to keep track of every row to know where to insert every column (1 search result).
-        generateRow('product-row-num-' + rowNum)
-        counter = 0
-        rowNum++
-      }
-
       //Create new product image and title. Four are occupied per row.
       generateColumn(arr.image, arr.name, rowNum - 1)
       counter++
     })
   })
-
-  function generateRow(idNum) {
-    $.get('/components/product-row.html', function (data) {
-      const productRow = $.parseHTML(data)
-      $(productRow).attr('id', idNum)
-      $('#products-list').append(productRow)
-    })
-  }
 
   function generateColumn(imgURL, Title, rowNum) {
     $.get('/components/product-list.html', function (data) {
@@ -33,7 +17,7 @@ function getProductResults() {
       $(productList).find('.product-name').html(Title)
 
       // Adds the column to the current available row with a space available (up to 4 per row)
-      $('#product-row-num-' + rowNum).append(productList)
+      $('#products-list').append(productList)
     })
   }
 }

da0d20c53d0513b9d4818d10c06291f422607da0
Osama <osamaazar48@hotmail.com>
2023-10-21 16:15:10 -0400

    Task 18/21 complete and functional yeehaw

--- public/js/products.js
+++ public/js/products.js
@@ -1,6 +1,3 @@
-let currentLocation = ''
-let searchTerm = ''
-
 async function getProductResults(category = '', search = '') {
   const products = await productService.findProducts(category, search)
   const productTemplate = await $.get('/components/product-list.html')
@@ -13,39 +10,35 @@ function generateColumn(template, imgURL, Title) {
   const productList = $.parseHTML(template)
   $(productList).find('.image-placeholder').attr('src', imgURL)
   $(productList).find('.product-name').html(Title)
-
   $('#products-list').append(productList)
 }
 
-function getCurrentLocation() {
+function reloadWebpageWithFilters() {
   let params = new URLSearchParams(document.location.search)
-  let category = params.get('category')
-  debugger
+  let category = params.get('category') || '' // If no category is present in the URL, category = ''
   let searchValue = String($('#search-input').val())
-  if (category !== '' && searchValue === '') return
+  if (category !== '' && searchValue === '') return // Since clicking on a category redirects to a pre-determiend URL (in the HTML <a>), this function will do nothing.
   if (category !== '' && searchValue !== '')
     location.href = `/products?category=${category}&search=${searchValue}`
   if (searchValue === '' && category === '') return
   if (searchValue !== '' && category === '')
-    location.href = `/products?search=${searchTerm}`
+    location.href = `/products?search=${searchValue}`
 }
 
-function searchByCategory() {
-  debugger
-  let params = new URLSearchParams(document.location.search)
-  let category = params.get('category') || ''
+function retrieveFiltersFromURL() {
+  // Retrieve category and search filters from the url
+  let params = new URLSearchParams(document.location.search) // Fetches current URL
+  let category = params.get('category') || '' // Stores the value from the url after "category=" and stops at & if present. If category isn't present, category = ''
   let search = params.get('search') || ''
-  console.log('category: ' + category)
-  console.log('searchTerm: ' + searchTerm)
-  console.log('currentLocation: ' + currentLocation)
+  // Fetches products with the category and search filters
   getProductResults(category, search)
 }
 
 function onSubmit(e) {
-  getCurrentLocation()
+  reloadWebpageWithFilters()
   return false
 }
 
 $('#form').submit(onSubmit)
 
-searchByCategory()
+retrieveFiltersFromURL()
--- public/js/services/product-service.js
+++ public/js/services/product-service.js
@@ -4,6 +4,7 @@ class ProductService {
     let filteredProducts = []
 
     if (category !== '' && search !== '') {
+      //If category and search filters are present.
       products.forEach((product) => {
         if (
           category === product.category &&
@@ -16,6 +17,7 @@ class ProductService {
     }
 
     if (category !== '') {
+      // If category is present, but not search filter.
       products.forEach((product) => {
         if (category === product.category) filteredProducts.push(product)
       })
@@ -23,6 +25,7 @@ class ProductService {
     }
 
     if (search !== '') {
+      // If search filter is present, but not category.
       products.forEach((product) => {
         if (product.name.toLowerCase().includes(search.toLowerCase()))
           filteredProducts.push(product)

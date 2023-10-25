9210699dd180e0a71b4097e5bf780007e99dda76
pratyushashukla <pratyusha.shukla212@gmail.com>
2023-10-25 03:08:06 -0400

    pt1- findng & dividng page nos & dynmc pagntn html

--- public/js/products.js
+++ public/js/products.js
@@ -1,9 +1,42 @@
 async function getProductResults() {
+  const productsPerPage = 5
+  const productsLimit = 0
   const products = await productService.findProducts()
-  const productTemplate = await $.get('/components/product-list.html')
-  products.forEach((product) =>
-    generateColumn(productTemplate, product.id, product.image, product.name)
-  )
+  if (products.length > 0) {
+    if (products.length > 5) {
+      const totalPages = Math.ceil(products.length / productsPerPage)
+      document.getElementById('pagination').hidden = false
+      this.productsLimit = 4
+
+      $('#pagination .pagination').append(
+        '<li class="page-item"> <a class="page-link" href="#" aria-label="Previous"> <span aria-hidden="true">&laquo;</span></a></li>'
+      )
+      for (let i = 0; i < totalPages; i++) {
+        $('#pagination .pagination').append(
+          `<li class="page-item"><a class="page-link" href="#" data-page="${
+            i + 1
+          }"> ${i + 1} </a></li>`
+        )
+      }
+      $('#pagination .pagination').append(
+        '<li class="page-item"> <a class="page-link" href="#" aria-label="Previous"> <span aria-hidden="true">&raquo;</span></a></li>'
+      )
+
+      //$('#pagination li').first().find('a').addClass('current')
+    } else {
+      document.getElementById('pagination').hidden = false
+    }
+
+    const productTemplate = await $.get('/components/product-list.html')
+    for (let i = 0; i <= this.productsLimit; i++) {
+      generateColumn(
+        productTemplate,
+        products[i].id,
+        products[i].image,
+        products[i].name
+      )
+    }
+  }
 }
 
 function generateColumn(template, id, imgURL, Title) {

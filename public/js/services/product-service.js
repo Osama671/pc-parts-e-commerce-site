61ad820fe314f41c6d87fe79d3d622b2440514c3
Osama <osamaazar48@hotmail.com>
2023-11-03 13:40:46 -0400

    Forgot to remove old code

--- public/js/services/product-service.js
+++ public/js/services/product-service.js
@@ -25,37 +25,6 @@ class ProductService {
     }
 
     return products
-
-    // if (category !== '' && search !== '') {
-    //   //If category and search filters are present.
-    //   products.forEach((product) => {
-    //     if (
-    //       category === product.category &&
-    //       product.name.toLowerCase().includes(search.toLowerCase()) === true
-    //     ) {
-    //       filteredProducts.push(product)
-    //     }
-    //   })
-    //   return filteredProducts
-    // }
-
-    // if (category !== '') {
-    //   // If category is present, but not search filter.
-    //   products.forEach((product) => {
-    //     if (category === product.category) filteredProducts.push(product)
-    //   })
-    //   return filteredProducts
-    // }
-
-    // if (search !== '') {
-    //   // If search filter is present, but not category.
-    //   products.forEach((product) => {
-    //     if (product.name.toLowerCase().includes(search.toLowerCase()))
-    //       filteredProducts.push(product)
-    //   })
-    //   return filteredProducts
-    // }
-    // return products
   }
 }
 

819deced50b4a03ccb56a5b480f84b4c027c9f7a
Osama <osamaazar48@hotmail.com>
2023-11-03 13:39:46 -0400

    Pull request changes completed succesfully

--- public/js/services/product-service.js
+++ public/js/services/product-service.js
@@ -1,38 +1,61 @@
 class ProductService {
   async findProducts(category = '', search = '') {
-    const products = await $.getJSON('/api/products.json')
-    let filteredProducts = []
+    let products = await $.getJSON('/api/products.json')
+    let filters = []
 
-    if (category !== '' && search !== '') {
-      //If category and search filters are present.
-      products.forEach((product) => {
-        if (
-          category === product.category &&
-          product.name.toLowerCase().includes(search.toLowerCase()) === true
-        ) {
-          filteredProducts.push(product)
-        }
-      })
-      return filteredProducts
+    const CategoryFilter = function (category) {
+      return (products) => products.category === category
     }
 
-    if (category !== '') {
-      // If category is present, but not search filter.
-      products.forEach((product) => {
-        if (category === product.category) filteredProducts.push(product)
-      })
-      return filteredProducts
+    const SearchFilter = function (search) {
+      return (products) =>
+        products.name.toLowerCase().includes(search.toLowerCase())
     }
 
-    if (search !== '') {
-      // If search filter is present, but not category.
-      products.forEach((product) => {
-        if (product.name.toLowerCase().includes(search.toLowerCase()))
-          filteredProducts.push(product)
-      })
-      return filteredProducts
+    if (category) {
+      filters.push(CategoryFilter(category))
     }
+
+    if (search) {
+      filters.push(SearchFilter(search))
+    }
+
+    for (let filter of filters) {
+      products = products.filter(filter)
+    }
+
     return products
+
+    // if (category !== '' && search !== '') {
+    //   //If category and search filters are present.
+    //   products.forEach((product) => {
+    //     if (
+    //       category === product.category &&
+    //       product.name.toLowerCase().includes(search.toLowerCase()) === true
+    //     ) {
+    //       filteredProducts.push(product)
+    //     }
+    //   })
+    //   return filteredProducts
+    // }
+
+    // if (category !== '') {
+    //   // If category is present, but not search filter.
+    //   products.forEach((product) => {
+    //     if (category === product.category) filteredProducts.push(product)
+    //   })
+    //   return filteredProducts
+    // }
+
+    // if (search !== '') {
+    //   // If search filter is present, but not category.
+    //   products.forEach((product) => {
+    //     if (product.name.toLowerCase().includes(search.toLowerCase()))
+    //       filteredProducts.push(product)
+    //   })
+    //   return filteredProducts
+    // }
+    // return products
   }
 }
 

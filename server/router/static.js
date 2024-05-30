bb83f7ad404d5a9a06d079897e7918f1871eb5a1
George Varghese M <george@georgevm.com>
2024-05-29 21:25:52 -0400

    Handle possibility of files being in `public` (dev) and `dist` (prod)

--- server/router/static.js
+++ server/router/static.js
@@ -1,15 +1,27 @@
 import * as express from 'express'
 import { createReadStream } from 'node:fs'
+import { stat } from 'node:fs/promises'
 
 const router = express.Router()
 
-const staticFiles = express.static('dist', { extensions: ['html', 'htm'] })
+router.use(express.static('dist', { extensions: ['html', 'htm'] }))
+router.use(express.static('public', { extensions: ['html', 'htm'] }))
 
-router.use(staticFiles)
+const index = (await doesFileExist('dist/index.html'))
+  ? 'dist/index.html'
+  : 'server/index.html'
 
-router.get('*', (req, res) => {
+router.get('*', async (req, res) => {
   res.header('Content-Type', 'text/html; charset=utf-8')
-  createReadStream('dist/index.html').pipe(res)
+  createReadStream(index).pipe(res)
 })
+
+async function doesFileExist(path) {
+  try {
+    return (await stat(path)).isFile()
+  } catch (e) {
+    return false
+  }
+}
 
 export default router

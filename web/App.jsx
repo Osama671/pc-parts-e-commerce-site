3e726f78c171ae8d976aebf47ad180edcdfc098f
Sharookhan Basheer <sharookhanb@gmail.com>
2024-06-01 14:12:14 -0400

    Added a home page and routed to /'

--- web/App.jsx
+++ web/App.jsx
@@ -1,11 +1,16 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import { FourOhFour } from './pages/404.jsx'
+import { Home } from './pages/home.jsx'
 
 const router = createBrowserRouter([
   {
     path: '*',
     element: <FourOhFour />,
+  },
+  {
+    path: '/',
+    element: <Home />,
   },
 ])
 
---
+++ web/pages/home.jsx
@@ -0,0 +1,10 @@
+export function Home() {
+  return (
+    <div className="container-fluid py-3" style={{ minHeight: '50vh' }}>
+      <h1 className="text-center" style={{ fontSize: '60px' }}>
+        Home
+      </h1>
+      <h2 className="text-center">This is my home page</h2>
+    </div>
+  )
+}

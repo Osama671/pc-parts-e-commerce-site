ccc7594f20a02983750e3ec86c8532de37dcd757
Osama671 <osamaazar48@hotmail.com>
2024-07-26 08:41:41 -0400

    Refactored code

--- server/repositories/orders.repository.js
+++ server/repositories/orders.repository.js
@@ -1,42 +1,22 @@
-import { ObjectId } from 'mongodb'
+import { clearCart } from '../services/cart.service.js'
 import { db } from './mongo.js'
 
 const ordersCollection = db.collection('orders')
-const cartCollection = db.collection('cart')
 ordersCollection.createIndex('orderId')
 
-export async function checkout(userId) {
-  const user = await ordersCollection.findOne({ userId })
-  if (!user) console.error('User not found')
-
-  if ((await cartCollection.findOne({ userId: userId })) === null) {
-    return console.error('User has no items in their cart.')
-  }
-
+export async function checkout(userId, products) {
   try {
-    const orderId = new ObjectId().toString()
-    let newProducts = await cartCollection.find({ userId: userId }).toArray()
-
-    newProducts.forEach((product) => ({
-      product_id: product.product_id,
-      quantity: product.quantity,
-    }))
-    await ordersCollection.insertOne({
-      _id: orderId,
-      userId: userId,
-      products: newProducts,
-    })
-    await clearCartAfterCheckout(userId)
-    return { orderId: orderId }
+    let insertedOrder = await insertOrder(userId, products)
+    await clearCart(userId)
+    return { orderId: insertedOrder.insertedId }
   } catch (error) {
-    console.error('Error during checkout', error)
+    console.error('Checkout failed')
   }
 }
 
-async function clearCartAfterCheckout(userId) {
-  try {
-    return await cartCollection.deleteMany({ userId: userId })
-  } catch (error) {
-    console.error('Error deleting cart items', error)
-  }
+export async function insertOrder(userId, products) {
+  return await ordersCollection.insertOne({
+    userId: userId,
+    products: products,
+  })
 }
--- server/services/orders.service.js
+++ server/services/orders.service.js
@@ -1,10 +1,20 @@
 import * as ordersRepository from '../repositories/orders.repository.js'
+import { getCart } from './cart.service.js'
 
 export async function checkout(userId) {
   try {
-    const orderId = await ordersRepository.checkout(userId)
+    let cartProducts = await getCart(userId)
+    if (cartProducts.length === 0) {
+      throw new Error('User has no items in their cart.')
+    }
+
+    cartProducts.forEach((product) => ({
+      product_id: product.product_id,
+      quantity: product.quantity,
+    }))
+    let orderId = ordersRepository.checkout(userId, cartProducts)
     return orderId
   } catch (error) {
-    console.error('Error in POST /cart/checkout:', error)
+    console.error('Error in POST /cart/checkout:', error.message)
   }
 }
